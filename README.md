9c046aecea870a92254607cefa0456c49820c46b
cjc77 <carsonjamescook@gmail.com>
2020-07-23 12:29:19 -0700

    Updated README and verification script.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## Developer configuration file
 Make sure you have a file, "dev_paths.txt", in the root project directory laid out as follows:
 ```
-root/path/for/storing/data (e.g. D:/unified_TCGA_GTEx)
+root/path/for/storing/data
 <newline>
 ```
 
--- verify_dirs.py
+++ verify_dirs.py
@@ -1,9 +1,19 @@
 import os
+import sys
+
 
 all_present = True
 
+
+# Check existence of "dev_paths.txt" file
+if not os.path.exists("dev_paths.txt"):
+    print(f"[ERROR] Could not find: dev_paths.txt")
+    print(f"You must create this file with the specifications listed in README.md.")
+    sys.exit("Please create this file and try again.")
+
 with open("dev_paths.txt") as f:
     data_dir = f.readline().strip()
+
 
 # Check existence of cervical cancer data
 if not os.path.exists(f"{data_dir}/unified_cervical_data/coldata.tsv"):
