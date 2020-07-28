0e75d79768dd50af09f86de32b78e5c6acdf35f7
cjc77 <carsonjamescook@gmail.com>
2020-07-27 17:10:06 -0700

    Updated verification script to match new dev_paths.txt protocol.

--- verify_dirs.py
+++ verify_dirs.py
@@ -3,40 +3,63 @@ import sys
 
 
 all_present = True
+cervical_dir = "unified_cervical_data"
+matrisome_dir = "matrisome"
 
 
-# Check existence of "dev_paths.txt" file
-if not os.path.exists("dev_paths.txt"):
-    print(f"[ERROR] Could not find: dev_paths.txt")
+dev_paths_file = "dev_paths.txt"
+counts_file = "counts.tsv"
+coldata_file = "coldata.tsv"
+matrisome_masterlist_file = "matrisome_hs_masterlist.tsv"
+
+
+# Check existence of dev_paths_file
+if not os.path.exists(dev_paths_file):
+    print(f"[FAILURE] Could not find: {dev_paths_file}")
     print(f"You must create this file with the specifications listed in README.md.")
     sys.exit("Please create this file and try again.")
+else:
+    print(f"[SUCCESS] Found: {dev_paths_file}")
 
 with open("dev_paths.txt") as f:
-    data_dir = f.readline().strip()
-
+    dirs = tuple(fn.strip() for fn in f.readlines())
+    data_dir, analysis_dir, figures_dir = dirs
 
 # Check existence of cervical cancer data
-if not os.path.exists(f"{data_dir}/unified_cervical_data/coldata.tsv"):
-    print(f"[ERROR] Could not find: {data_dir}/unified_cervical_data/coldata.tsv")
+if not os.path.exists(f"{data_dir}/{cervical_dir}/{coldata_file}"):
+    print(f"[FAILURE] Could not find: {data_dir}/{cervical_dir}/{coldata_file}")
     all_present = False
-if not os.path.exists(f"{data_dir}/unified_cervical_data/counts.tsv"):
-    print(f"[ERROR] Could not find: {data_dir}/unified_cervical_data/counts.tsv")
+else:
+    print(f"[SUCCESS] Found: {data_dir}/{cervical_dir}/{coldata_file}")
+
+if not os.path.exists(f"{data_dir}/{cervical_dir}/{counts_file}"):
+    print(f"[FAILURE] Could not find: {data_dir}/{cervical_dir}/{counts_file}")
     all_present = False
+else:
+    print(f"[SUCCESS] Found: {data_dir}/{cervical_dir}/{counts_file}")
 
 # Check existence of matrisome data
-if not os.path.exists(f"{data_dir}/matrisome/matrisome_hs_masterlist.tsv"):
-    print(f"[ERROR] Could not find: {data_dir}/matrisome/matrisome_hs_masterlist.tsv")
+if not os.path.exists(f"{data_dir}/{matrisome_dir}/{matrisome_masterlist_file}"):
+    print(f"[FAILURE] Could not find: {data_dir}/{matrisome_dir}/{matrisome_masterlist_file}")
     all_present = False
+else:
+    print(f"[SUCCESS] Found: {data_dir}/{matrisome_dir}/{matrisome_masterlist_file}")
 
 # Check existence of analysis directory
-if not os.path.exists(f"{data_dir}/analysis"):
-    os.makedirs(f"{data_dir}/analysis")
-    print(f"Created directory: {data_dir}/analysis")
+if not os.path.exists(analysis_dir):
+    os.makedirs(analysis_dir)
+    print(f"[SUCCESS] Created analysis directory: {analysis_dir}")
+else:
+    print(f"[SUCCESS] Analysis directory already exists.")
 
 # Check existence of figures directory
-if not os.path.exists(f"{data_dir}/figures"):
-    os.makedirs(f"{data_dir}/figures")
-    print(f"Created directory: {data_dir}/figures")
+if not os.path.exists(figures_dir):
+    os.makedirs(figures_dir)
+    print(f"[SUCCESS] Created figures directory: {figures_dir}")
+else:
+    print(f"[SUCCESS] Figures directory already exists.")
 
 if all_present:
-    print(f"[SUCCESS] All necessary data/directories present.")
+    print(f"[ALL SUCCEEDED] All necessary data/directories present.")
+else:
+    print(f"[FAILURES PRESENT] Check messages and make sure all needed directories/files are present.")
