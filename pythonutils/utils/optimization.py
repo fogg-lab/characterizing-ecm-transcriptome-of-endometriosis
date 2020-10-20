94b125ec8af31b79f83f2745665ccdef350df96e
cjc77 <carsonjamescook@gmail.com>
2020-10-19 19:37:43 -0700

    Retrained GBR. Need to update callback code to protect against identical hyper parameters being saved multiple times.

--- pythonutils/utils/optimization.py
+++ pythonutils/utils/optimization.py
@@ -13,14 +13,38 @@ scoring_metrics = {
 }
 
 
+# def save_callback(res: OptimizeResult, dest: str, sep: str = "\t") -> None:
+#     # Does this model correspond with the best score seen so far?
+#     if res.func_vals[-1] == res.fun:
+#         h_param_df = pd.DataFrame({
+#             "param": res.space.dimension_names + ["loss_achieved"],
+#             "param_value": res.x + [res.fun]
+#         })
+#         h_param_df.to_csv(dest, sep=sep, index=False)
+
+
 def save_callback(res: OptimizeResult, dest: str, sep: str = "\t") -> None:
-    # Does this model correspond with the best score seen so far?
-    if res.func_vals[-1] == res.fun:
-        h_param_df = pd.DataFrame({
-            "param": res.space.dimension_names + ["loss_achieved"],
-            "param_value": res.x + [res.fun]
-        })
-        h_param_df.to_csv(dest, sep=sep, index=False)
+    # Hyper-parameters from most recent model
+    new_h_param_df = pd.DataFrame(
+        columns=res.space.dimension_names + ["loss_achieved"],
+        data=[res.x + [res.func_vals[-1]]]
+    )
+    
+    try:
+        # Existing optimal hyper-parameters
+        h_param_df = pd.read_csv(dest, sep=sep)
+        
+        # Not yet 5 models? Add the new one
+        if h_param_df.shape[0] < 5:
+            h_param_df = pd.concat([h_param_df, new_h_param_df], axis=0)
+        # New model better than the worst of previous 5 best? Replace it
+        elif res.func_vals[-1] < h_param_df.loss_achieved.max():
+            h_param_df.iloc[[h_param_df.loss_achieved.argmax()]] = new_h_param_df.values
+    except FileNotFoundError:
+        # First model? Save results
+        h_param_df = new_h_param_df
+    
+    h_param_df.to_csv(dest, sep=sep, index=False)
 
 
 def cv_permutation_importance(estimator: object, x_df: pd.DataFrame, y_df: pd.DataFrame, metric: str, k: int = 5, random_state: Optional[RandomState] = None, n_repeats: int = 11) -> Tuple[List, np.ndarray]:
