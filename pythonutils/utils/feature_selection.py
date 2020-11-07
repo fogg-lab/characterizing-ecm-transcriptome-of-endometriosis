355b715fc5121a54c2c3109f8bb0c025a9aae92b
cjc77 <carsonjamescook@gmail.com>
2020-11-06 22:15:03 -0800

    Pushed from wrong directory.

--- pythonutils/utils/feature_selection.py
+++ pythonutils/utils/feature_selection.py
@@ -14,3 +14,19 @@ def gather_perm_res(x_df: pd.DataFrame, mean_importances: np.ndarray, ref_score_
             .reset_index(drop=True)
     )
     return perm_res_df
+
+# Adapted from Mehmood, T. et al.: https://www.sciencedirect.com/science/article/pii/S0169743912001542
+def VIP(plsr_model):
+    T = plsr_model.x_scores_
+    W = plsr_model.x_weights_
+    Q = plsr_model.y_loadings_
+    p, a = W.shape
+    
+    vip = np.zeros(p)
+    # SSa for each A
+    SSA = np.sum(T ** 2, axis=0) * np.sum(Q ** 2, axis=0)
+    # Column-wise l2 norm of W
+    W_norm = np.einsum("ij, ij -> j", W, W)
+    
+    vip = np.sqrt(p * np.sum(SSA * (W / W_norm) ** 2, axis=1) / np.sum(SSA, axis=0))
+    return vip
