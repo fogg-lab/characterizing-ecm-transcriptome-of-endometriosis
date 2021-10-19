6fb168c88b74030dce227af0beee5317beee711b
cjc77 <carsonjamescook@gmail.com>
2021-10-19 11:42:55 -0700

    Updated verify dirs script using Ajay's edits with modifications.

--- verify_dirs.py
+++ verify_dirs.py
@@ -38,6 +38,9 @@ with open("dev_paths.txt") as f:
     dirs = tuple(fn.strip() for fn in f.readlines())
     data_dir, analysis_dir, figures_dir = dirs
 
+
+#### DATA ####
+
 # Check existence of cancer data
 for dset in cancer_dsets:
     if not os.path.exists(f"{data_dir}/{dset}/{coldata_file}"):
@@ -52,22 +55,21 @@ for dset in cancer_dsets:
     else:
         print(f"{SFM.success} Found: {data_dir}/{dset}/{counts_file}")
 
+data_array = ["matrisome", "raw_unified_TCGA_GTEx", "saved_network_objects", "saved_RSE_objects", "tcga_biolinks_downloads", "TCGA_RNA_combined_matrix_count_data", "TCGA_RNA_matrix_count_data", "unified_cervical_data", "unified_uterine_data", "unified_uterine_endometrial_data"]
 
-# Check existence of matrisome data
-if not os.path.exists(f"{data_dir}/{matrisome_dir}/{matrisome_masterlist_file}"):
-    print(f"{SFM.failure} Could not find: {data_dir}/{matrisome_dir}/{matrisome_masterlist_file}")
-    all_present = False
-else:
-    print(f"{SFM.success} Found: {data_dir}/{matrisome_dir}/{matrisome_masterlist_file}")
+data_length = len(data_array)
 
-# Check existence of pathology data
-if not os.path.exists(f"{data_dir}/THPA_v20_1_staining/pathology.tsv"):
-    print(f"{SFM.failure} Could not find: {data_dir}/THPA_v20_1_staining/pathology.tsv")
-    all_present = False
-else:
-    print(f"{SFM.success} Found: {data_dir}/THPA_v20_1_staining/pathology.tsv")
+for i in range(data_length):
+    if os.path.exists(f"{data_dir}/{data_array[i]}"):
+        print(f"{SFM.success} Data : {data_array[i]} exists")
+    else: 
+        print(f"{SFM.failure} {data_array[i]} Data : does not exist*")
+        os.makedirs(f"{data_dir}/{data_array[i]}")
+        print(f"{SFM.success} Created data/{data_array[i]} {data_dir}/{data_array[i]}")
+        print(f"\tWARNING: directory created, but data may be missing.")
 
 
+#### ANALYSIS ####
 
 # Check existence of analysis directory
 if not os.path.exists(analysis_dir):
@@ -76,62 +78,21 @@ if not os.path.exists(analysis_dir):
 else:
     print(f"{SFM.success} Analysis directory already exists.")
 
-# Check existence of analysis/deg directory
-if not os.path.exists(f"{analysis_dir}/deg"):
-    os.makedirs(f"{analysis_dir}/deg")
-    print(f"{SFM.success} Created analysis/deg directory: {analysis_dir}/deg")
-else:
-    print(f"{SFM.success} Analysis deg directory already exists.")
+analysis_array = ["deg", "model_opt", "feature_selection", "meta", "network", "enrichment", "gene_lists", "gene_lists_extra", "gene_lists_extra_network", "survival", "manual_notes", "one_off"]
 
-# Check existence of analysis/model_opt directory
-if not os.path.exists(f"{analysis_dir}/model_opt"):
-    os.makedirs(f"{analysis_dir}/model_opt")
-    print(f"{SFM.success} Created analysis/model_opt directory: {analysis_dir}/model_opt")
-else:
-    print(f"{SFM.success} Analysis model_opt directory already exists.")
+analysis_length = len(analysis_array)
 
-# Check existence of analysis/feature_selection directory
-if not os.path.exists(f"{analysis_dir}/feature_selection"):
-    os.makedirs(f"{analysis_dir}/feature_selection")
-    print(f"{SFM.success} Created analysis/feature_selection directory: {analysis_dir}/feature_selection")
-else:
-    print(f"{SFM.success} Analysis feature_selection directory already exists.")
+for i in range(analysis_length):
+    if os.path.exists(f"{analysis_dir}/{analysis_array[i]}"):
+        print(f"{SFM.success} Analysis :  {analysis_array[i]} exists")
+    else: 
+        print(f"{SFM.failure} Analysis : {analysis_array[i]} does not exist*")
+        os.makedirs(f"{analysis_dir}/{analysis_array[i]}")
+        print(f"{SFM.success} Created analysis/{analysis_array[i]} directory: {analysis_dir}/{analysis_array[i]}")
+        print(f"\tWARNING: directory created, but data may be missing.")
 
-# Check existence of analysis/meta directory
-if not os.path.exists(f"{analysis_dir}/meta"):
-    os.makedirs(f"{analysis_dir}/meta")
-    print(f"{SFM.success} Created analysis/meta directory: {analysis_dir}/meta")
-else:
-    print(f"{SFM.success} Analysis meta directory already exists.")
 
-# Check existence of analysis/network directory
-if not os.path.exists(f"{analysis_dir}/network"):
-    os.makedirs(f"{analysis_dir}/network")
-    print(f"{SFM.success} Created analysis/network directory: {analysis_dir}/network")
-else:
-    print(f"{SFM.success} Analysis network directory already exists.")
-
-# Check existence of analysis/enrichment directory
-if not os.path.exists(f"{analysis_dir}/enrichment"):
-    os.makedirs(f"{analysis_dir}/enrichment")
-    print(f"{SFM.success} Created analysis/enrichment directory: {analysis_dir}/enrichment")
-else:
-    print(f"{SFM.success} Analysis enrichment directory already exists.")
-
-# Check existence of analysis/gene_lists directory
-if not os.path.exists(f"{analysis_dir}/gene_lists"):
-    os.makedirs(f"{analysis_dir}/gene_lists")
-    print(f"{SFM.success} Created analysis/gene_lists directory: {analysis_dir}/gene_lists")
-else:
-    print(f"{SFM.success} Analysis gene_lists directory already exists.")
-
-# Check existence of analysis/survival directory
-if not os.path.exists(f"{analysis_dir}/survival"):
-    os.makedirs(f"{analysis_dir}/survival")
-    print(f"{SFM.success} Created analysis/survival directory: {analysis_dir}/survival")
-else:
-    print(f"{SFM.success} Analysis survival directory already exists.")
-
+#### Figures ####
 
 # Check existence of figures directory
 if not os.path.exists(figures_dir):
@@ -140,57 +101,21 @@ if not os.path.exists(figures_dir):
 else:
     print(f"{SFM.success} Figures directory already exists.")
 
-# Check existence of figures/TCGA_overview directory
-if not os.path.exists(f"{figures_dir}/TCGA_overview"):
-    os.makedirs(f"{figures_dir}/TCGA_overview")
-    print(f"{SFM.success} Created figures/TCGA_overview directory: {figures_dir}/TCGA_overview")
-else:
-    print(f"{SFM.success} Figures TCGA_overview directory already exists.")
+figures_array = ["TCGA_overview", "network", "deg", "enrichment", "gene_lists", "models", "one_off", "paneled", "saved_obj"]
 
-# Check existence of figures/network directory
-if not os.path.exists(f"{figures_dir}/network"):
-    os.makedirs(f"{figures_dir}/network")
-    print(f"{SFM.success} Created figures/network directory: {figures_dir}/network")
-else:
-    print(f"{SFM.success} Figures network directory already exists.")
+figures_length = len(figures_array)
 
-# Check existence of figures/deg directory
-if not os.path.exists(f"{figures_dir}/deg"):
-    os.makedirs(f"{figures_dir}/deg")
-    print(f"{SFM.success} Created figures/deg directory: {figures_dir}/deg")
-else:
-    print(f"{SFM.success} Figures deg directory already exists.")
-
-# Check existence of figures/enrichment directory
-if not os.path.exists(f"{figures_dir}/enrichment"):
-    os.makedirs(f"{figures_dir}/enrichment")
-    print(f"{SFM.success} Created figures/enrichment directory: {figures_dir}/enrichment")
-else:
-    print(f"{SFM.success} Figures enrichment directory already exists.")
-
-# Check existence of figures/gene_lists directory
-if not os.path.exists(f"{figures_dir}/gene_lists"):
-    os.makedirs(f"{figures_dir}/gene_lists")
-    print(f"{SFM.success} Created figures/gene_lists directory: {figures_dir}/gene_lists")
-else:
-    print(f"{SFM.success} Figures gene_lists directory already exists.")
-
-# Check existence of figures/models directory
-if not os.path.exists(f"{figures_dir}/models"):
-    os.makedirs(f"{figures_dir}/models")
-    print(f"{SFM.success} Created figures/models directory: {figures_dir}/models")
-else:
-    print(f"{SFM.success} Figures models directory already exists.")
+for i in range(figures_length):
+    if os.path.exists(f"{figures_dir}/{figures_array[i]}"):
+        print(f"{SFM.success} Figures : {figures_array[i]} exists")
+    else: 
+        print(f"{SFM.failure} Figures : {figures_array[i]} does not exist*")
+        os.makedirs(f"{figures_dir}/{figures_array[i]}")
+        print(f"{SFM.success} Created figures/{figures_array[i]} directory: {figures_dir}/{figures_array[i]}")
+        print(f"\tWARNING: directory created, but data may be missing.")
 
 
-# Check existence of figures/one_off directory
-if not os.path.exists(f"{figures_dir}/one_off"):
-    os.makedirs(f"{figures_dir}/one_off")
-    print(f"{SFM.success} Created figures/one_off directory: {figures_dir}/one_off")
-else:
-    print(f"{SFM.success} Figures one_off directory already exists.")
-
-
+#### Final check ####
 if all_present:
     print(f"{SFM.all_succeeded} All necessary data/directories present.")
 else:
