ebe277cebfb658581ae70b31705089e1b4eef14f
Noah Wiggin <wigginno@oregonstate.edu>
2023-06-27 13:42:16 -0400

    prepare sample conditions (coldata) in prep notebook

--- data_prep/rma_counts.r
+++ data_prep/rma_counts.r
@@ -1,5 +1,6 @@
 library(affy)
 library(Biobase)
+library(GEOquery)
 
 args <- commandArgs(trailingOnly=TRUE)
 
@@ -7,6 +8,36 @@ data_dir <- args[1]
 output_dir <- args[2]
 
 series_dirs <- list.dirs(data_dir, recursive=FALSE)
+
+saveCounts <- function(expr_df, series) {
+    output_file = paste0(output_dir, "/", series, "_counts_unmapped.tsv")
+    write.table(counts, file=output_file, sep="\t", quote=F, row.names=FALSE)
+}
+
+saveColdata <- function(coldata, series) {
+    # Keep only "sample_name" and "condition" columns
+    coldata <- coldata[, c("sample_name", "condition")]
+
+    output_file = paste0(output_dir, "/", series, "_coldata.tsv")
+    write.table(coldata, file=output_file, sep="\t", quote=F, row.names = FALSE)
+}
+
+processColdata <- function(coldata) {
+    condition <- character(nrow(coldata))
+
+    for(i in 1:nrow(coldata)) {
+        sample_row <- tolower(paste(coldata[i, ], collapse=" "))
+        if(grepl("non-endometriosis|normal patient|disease status: healthy", sample_row)) {
+            condition[i] <- "healthy"
+        } else if(grepl("endometriosis severity|patient with\\s*endometriosis", sample_row)) {
+            condition[i] <- "endometriosis"
+        } else {
+            condition[i] <- "healthy"
+        }
+    }
+
+    return(data.frame(sample_name = row.names(coldata), condition = condition))
+}
 
 writeLines("Preprocessing data in the following directories:")
 writeLines(series_dirs)
@@ -33,7 +64,27 @@ for (series_dir in series_dirs) {
     # Add probe column
     expr_df <- data.frame(probe = rownames(expr_df), expr_df, row.names = NULL)
 
-    # Write to file
-    output_file = paste0(output_dir, "/", series, "_counts_unmapped.tsv")
-    write.table(expr_df, file=output_file, sep="\t", quote=F, row.names=FALSE)
+    # Write expression matrix to file
+    saveCounts(expr_df, series)
+
+    # Prepare coldata
+    out = tryCatch({
+        # Get GEO series and corresponding platform
+        gset <- getGEO(series, GSEMatrix=TRUE, getGPL=TRUE)
+        if (length(gset) > 1) {
+            idx <- grep(pattern = series, x = attr(gset, "names"))
+        } else idx <- 1
+        gset <- gset[[idx]]
+
+        # Get and process coldata
+        coldata <- gset@phenoData@data
+        coldata <- processColdata(coldata)
+
+        if (!is.null(coldata)) {
+            # Write coldata to file
+            saveColdata(coldata, series)
+        }
+    }, error = function(e) {
+        print(paste('error:', e))
+    })
 }
