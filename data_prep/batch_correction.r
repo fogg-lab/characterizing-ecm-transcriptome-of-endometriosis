ce28e56841c7402581f7a9164aa63842efe4af60
Noah Wiggin <77313083+wigginno@users.noreply.github.com>
2023-08-16 00:36:02 -0400

    Fix variable names in batch correction script

--- data_prep/batch_correction.r
+++ data_prep/batch_correction.r
@@ -5,27 +5,27 @@ library(sva)
 args = commandArgs(trailingOnly = TRUE)
 
 # Paths
-countspath = args[1]
-coldatapath = args[2]
+expression_file = args[1]
+clinical_file = args[2]
 
-# Read in the sample data and counts
-counts <- read_tsv(countspath)
-metadata <- read_tsv(coldatapath)
+# Read in the expression matrix and clinical data table
+expression_df <- read_tsv(expression_file)
+clinical_df <- read_tsv(clinical_file)
 
 # Prep batch correction
-rma_expr <- as.matrix(counts[-1])
-rownames(rma_expr) <- counts$hgnc_symbol
-model_m <- model.matrix(~ condition, data = metadata)
-batch <- metadata$batch
+expression_matrix <- as.matrix(expression_df[-1])
+rownames(expression_matrix) <- expression_df$hgnc_symbol
+model_m <- model.matrix(~ condition, data = clinical_df)
+batch <- clinical_df$batch
 
 # Get symbol column for adding to output later
-symbol <- counts$hgnc_symbol
+symbol <- expression_df$hgnc_symbol
 symbols <- data.frame(symbol)
 
 # Batch correction
-bc_rma_expr <- ComBat(rma_expr, batch = batch, mod = model_m, ref.batch = 1)
+bc_expression_matrix <- ComBat(expression_matrix, batch = batch, mod = model_m, ref.batch = 1)
 
 # Add symbol column and write the dataframe to file
-bc_rma_expr <- data.frame(bc_rma_expr)
-result <- bind_cols(symbols, bc_rma_expr)
-write_tsv(result, file.path(countspath))
+bc_expression_matrix <- data.frame(bc_expression_matrix)
+result <- bind_cols(symbols, bc_expression_matrix)
+write_tsv(result, file.path(expression_file))
